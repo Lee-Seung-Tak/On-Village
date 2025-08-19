932f66e0686ff851f90be6fd36c608ba42c75ce1
Ubuntu <lstlove9804@gmail.com>
2025-08-19 04:22:24 +0000

    lst add / debug 로그 삭제

--- grandparent_agent/generate/util/util.py
+++ grandparent_agent/generate/util/util.py
@@ -5,7 +5,6 @@ import os
 load_dotenv()
 
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
-print(AWS_ACCESS_KEY, flush=True)
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = "ap-northeast-2"  # 서울 리전
 
@@ -24,3 +23,21 @@ MAX_SILENCE_COUNT = 17
 vad = webrtcvad.Vad()
 vad.set_mode(0)  # 0~3, 민감도
 frame_size = int(SAMPLE_RATE * SAMPLE_WIDTH * CHUNK_DURATION_MS / 1000)
+
+
+# llm 관련
+import boto3
+from langchain_aws import ChatBedrock
+
+bedrock_client = boto3.client(
+    "bedrock-runtime",
+    region_name=AWS_REGION,   # 반드시 본인 리전에 맞게 변경
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY
+)
+
+# LangChain용 Bedrock 래퍼
+grandparent_agent = ChatBedrock(
+    client=bedrock_client,
+    model_id="us.anthropic.claude-3-haiku-20240307-v1:0"
+)
